d52fcf9241384cf39d9cba0428fbd5009461b69c
Andrey <aomatsiyuk@gmail.com>
2018-06-23 21:25:13 +0300

    Add solution

--- WinterAndSummer/Program.cs
+++ WinterAndSummer/Program.cs
@@ -10,6 +10,28 @@ namespace WinterAndSummer
     {
         static void Main(string[] args)
         {
+            var solution = new Solution();
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter temperatures separated by spaces");
+                var input = Console.ReadLine().Split(' ');
+                var temperatures = new int[input.Length];
+                for(int i = 0; i < input.Length; i++)
+                {
+                    if (!int.TryParse(input[i], out int temperature))
+                    {
+                        Console.WriteLine("There was an error in input data.");
+                        Console.ReadKey();
+                        return;
+                    }
+                    temperatures[i] = temperature;
+                }
+                var result = solution.WinterAndSummer(temperatures);
+                Console.WriteLine(result);
+                Console.Write("Again? (y/n)");
+
+            } while (Console.ReadKey(false).Key == ConsoleKey.Y);
         }
     }
 }
